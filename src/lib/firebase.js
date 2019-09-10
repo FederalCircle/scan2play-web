49137b90d362532ff6f92b016cccb304e069bae2
Alyson Maia <alyson@zeropay.io>
2019-09-10 03:02:04 -0300

    fix(firebase): fix .env values injection

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,14 +1,16 @@
 import firebase from 'firebase'
 
 const config = {
-  apiKey: process.env.REACT_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_FIREBASE_APP_ID
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 }
 
-export const firebaseImpl = firebase.initializeApp(config)
-export const firebaseDatabase = firebase.database()
+firebase.initializeApp(config)
+
+export const databaseRef = firebase.database().ref()
+export const testRef = databaseRef.child('test')
